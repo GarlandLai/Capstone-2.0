ddcde72e51feaee0c56f8a12c585fdf8887890bb
Garland Lai <garlandlai24@gmail.com>
2019-09-27 14:55:13 -0700

    add handlechange and submit to form

--- src/components/journals/CreateJournal.js
+++ src/components/journals/CreateJournal.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react';
 
 class CreateJournal extends Component {
+    state = {
+      title: '',
+      content: ''
+    }
   
+    handleChange = (e) => {
+      this.setState({
+        [e.target.id]: e.target.value
+      })
+    }
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+      this.props.createJournal(this.state)
+    }
+
   render() {
+    console.log(this.state);
     return (
       <div className="container">
-        <form>
+        <form onSubmit={this.handleSubmit} >
           <h4 className="grey-text text-darken-3">Meditation Journal</h4>
           <div className="input-field">
-            <label htmlFor="prompt1" id="prompt1">How do you feel about your meditation experience?</label>
-            <input type="text"/>
+            <label htmlFor="prompt1">How do you feel about your meditation experience?</label>
+            <input type="text" id="prompt1" onChange={this.handleChange} />
           </div>
 
           <div className="input-field">
-            <label htmlFor="prompt2" id="prompt2">What did you notice about your thoughts?</label>
-            <input type="text"/>
+            <label htmlFor="prompt2">What did you notice about your thoughts?</label>
+            <input type="text" id="prompt2" onChange={this.handleChange} />
           </div>
 
           <div className="input-field">
-            <label htmlFor="prompt3" id="prompt3">Did you notice any sensations or discomfort? Where?</label>
-            <input type="text"/>
+            <label htmlFor="prompt3">Did you notice any sensations or discomfort? Where?</label>
+            <input type="text" id="prompt3" onChange={this.handleChange} />
           </div>
 
           <div className="input-field">
-            <label htmlFor="content" id="content">This is where you can write anything you want!</label>
-            <textarea className="materialize-textarea"/>
+            <label htmlFor="content">This is where you can write anything you want!</label>
+            <textarea className="materialize-textarea"  id="content" onChange={this.handleChange} />
           </div>
           <div className="input-field">
             <button>Create</button>
