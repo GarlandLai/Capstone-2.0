3bb64717b0408a8111857ae3a059b8c31bcaed56
Garland Lai <garlandlai24@gmail.com>
2019-09-27 12:25:22 -0700

    add journal for meditiation

--- src/components/journals/CreateJournal.js
+++ src/components/journals/CreateJournal.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 
-class CreateProject extends Component {
+class CreateJournal extends Component {
   render() {
     return (
       <div className="container">
-        <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="grey-text text-darken-3">Create New Project</h5>
+        <form>
+          <h4 className="grey-text text-darken-3">Meditation Journal</h4>
           <div className="input-field">
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" onChange={this.handleChange} />
+            <label htmlFor="prompt">How do you feel about your meditation experience?</label>
+            <input type="text"/>
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="prompt">What did you notice about your thoughts?</label>
+            <input type="text"/>
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="prompt">Did you notice any sensations or discomfort? Where?</label>
+            <input type="text"/>
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="content">This is where you can write anything you want!</label>
+            <textarea className="materialize-textarea"/>
           </div>
           <div className="input-field">
-            <label htmlFor="content">Project Details</label>
-            <textarea className="materialize-textarea" id="content" onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
-            <button className="btn pink lighten-1 z-depth-0">Create</button>
+            <button>Create</button>
           </div>
         </form>
       </div>
@@ -23,10 +34,4 @@ class CreateProject extends Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    createProject: (project) => dispatch(createProject(project))
-  }
-}
-
-export default connect(null, mapDispatchToProps)(CreateProject);
+export default CreateJournal;
