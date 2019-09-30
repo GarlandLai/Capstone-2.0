688c246de4240f0cceb4ecd829588103b78e114e
Student <example@example.com>
2019-09-30 11:00:10 -0700

    list out the individual journal entries

--- src/components/journals/JournalList.js
+++ src/components/journals/JournalList.js
@@ -6,6 +6,16 @@ function JournalList(props) {
   console.log(props)
   return (
     <div className='container'>
+      {props.journalList.map(journalList => {
+        return (
+          <div className="single-list" key={journalList.id}>
+            <h4>{journalList.prompt1}</h4>
+            <h4>{journalList.prompt2}</h4>
+            <h4>{journalList.prompt3}</h4>
+            <h4>{journalList.content}</h4>
+          </div>
+        )
+      })}
       < JournalDetails />
     </div>
   );
