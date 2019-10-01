dffd6e2c3b1ddcea9b1365401262814f61307374
Student <example@example.com>
2019-10-01 11:00:43 -0700

    separate journallist and individual journals

---
+++ src/components/journals/Journal.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function Journal(props) {
+  console.log(props)
+  return (
+  <div>
+<h2 onClick={() => {alert("Clicked on Journal #" + props.id)}}>Journal Entry #{props.id}</h2>
+<h4>{props.prompt1}</h4>
+<h4>{props.prompt2}</h4>
+<h4>{props.prompt3}</h4>
+<h4>{props.content}</h4>
+</div>
+  );
+};
+
+Journal.propTypes = {
+  prompt1: PropTypes.string,
+  prompt2: PropTypes.string,
+  prompt3: PropTypes.string,
+  content: PropTypes.string,
+  id: PropTypes.string
+}
+
+export default Journal;
+
+
--- src/components/journals/JournalList.js
+++ src/components/journals/JournalList.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import JournalDetails from './JournalDetails';
+import Journal from './Journal';
 import PropTypes from 'prop-types';
 
 function JournalList(props) {
   console.log(props)
 
-  let selectEntryContent = null;
-  if (props.onSelectEntry != null) {
-    selectEntryContent = < JournalDetails />;
-  }
-
   return (
     <div className='container'>
-      {props.journalList.map(entry => {
-        return (
-
-          <div className="single-list" key={entry.id}>
-            <h2 onClick={() => {alert("Clicked on Journal #" + entry.id)}}>Journal #{entry.id} - {entry.created_at}</h2>
-            <h4>{entry.prompt1}</h4>
-            <h4>{entry.prompt2}</h4>
-            <h4>{entry.prompt3}</h4>
-            <h4>{entry.content}</h4>
-          </div>
-        )
-      })}
-
+      {props.journalList.map((journal) =>
+        <Journal prompt1={journal.prompt1}
+          prompt2={journal.prompt2}
+          prompt3={journal.prompt3}
+          content={journal.content}
+          id={journal.id} />
+      )}
     </div>
   );
 }
