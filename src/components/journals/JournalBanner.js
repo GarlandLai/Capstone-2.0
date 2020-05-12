e81a7a6d6c9f73f96d3daff8811a5ad76cc179fa
Garland Lai <garlandlai24@gmail.com>
2020-05-12 12:34:31 -0700

    use styled components in journalBanner

--- src/components/journals/JournalBanner.js
+++ src/components/journals/JournalBanner.js
@@ -1,21 +1,35 @@
 import React from 'react';
+import styled from "styled-components";
+
+const BannerContainer = styled.div`
+  font-weight: bold;
+  text-shadow: 4px 4px 8px black 
+`;
+
+const StyledLi = styled.p`
+  font-size: 18px;
+
+  :hover {
+    font-size: 28px;
+  }
+`;
+
+const H1 = styled.h1`
+  font-size: 45px;
+  text-decoration: underline;
+  padding-bottom: 10px;
+`;
+
 
 function JournalBanner() {
-
-  const bannerStyles = {
-    fontWeight: "bold",
-    textShadow: "4px 4px 8px black",
-  }
   return (
-    <div style={bannerStyles} id='journalBanner'>
-      <h1>Reflect to jumpstart your goals</h1>
-      <ul>
-        <li>Mindfulness to improve self awareness and emotional intelligence</li>
-        <li>Boost memory, comprehension, and creativity</li>
-        <li>Improve communication and self-confidence</li>
-        <li>Release stress and anxiety to improve sleep</li>
-      </ul>
-    </div>
+    <BannerContainer id='journalBanner'>
+      <H1>Reflect to jumpstart your goals</H1>
+      <StyledLi>Mindfulness to improve self awareness and emotional intelligence</StyledLi>
+      <StyledLi>Boost memory, comprehension, and creativity</StyledLi>
+      <StyledLi>Improve communication and self-confidence</StyledLi>
+      <StyledLi>Release stress and anxiety to improve sleep</StyledLi>
+    </BannerContainer>
   );
 }
 
