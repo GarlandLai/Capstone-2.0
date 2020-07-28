0e157126cfcc8bd388d57b1209a36baa18cb977d
Garland Lai <garlandlai24@gmail.com>
2020-07-28 16:09:47 -0700

    add some quick styling changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,80 +1,80 @@
 import React from 'react';
 import { Link } from 'react-scroll';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const CustomNav = styled.div`
-  background-color: grey;
-  width: 100vw;
-  height: 75px;
+	background-color: grey;
+	width: 100vw;
+	height: 75px;
 `;
 
 const StyledLink = styled(Link)`
-  font-size: 24px;
-  font-weight: 600;
-  padding: 0 100px;
-  height: 75px;
+	font-size: 24px;
+	font-weight: 600;
+	padding: 0 100px;
+	height: 75px;
 
-  :hover {
-    background-color: lightgrey;
-  }
+	:hover {
+		background-color: lightgrey;
+	}
 `;
 const StyledList = styled.div`
-display: inline-flex;
+	display: inline-flex;
 
-p {
-  padding-top: 20px;
-}
+	p {
+		padding-top: 20px;
+	}
 `;
 
 function Navigation() {
-  return ( 
-    <div>
-      <CustomNav>
-          <StyledList>
-            <StyledLink
-              activeClass="active"
-              to="header"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={1000}
-              >
-              <p>ZIAI HOME</p>
-            </StyledLink>
-            <StyledLink
-              activeClass="active"
-              to="about"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={1000}
-              >
-              <p>ABOUT</p>
-            </StyledLink>
-            <StyledLink
-              activeClass="active"
-              to="meditate"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={1000}
-              >
-              <p>MEDITATE</p>
-            </StyledLink>
-            <StyledLink
-              activeClass="active"
-              to="journals"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={1000}
-            >
-            <p>JOURNALS</p>
-          </StyledLink>
-        </StyledList>
-      </CustomNav>
-    </div>
-  );
+	return (
+		<div>
+			<CustomNav>
+				<StyledList>
+					<StyledLink
+						activeClass="active"
+						to="header"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={1000}
+					>
+						<p>HOME</p>
+					</StyledLink>
+					<StyledLink
+						activeClass="active"
+						to="about"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={1000}
+					>
+						<p>ABOUT</p>
+					</StyledLink>
+					<StyledLink
+						activeClass="active"
+						to="meditate"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={1000}
+					>
+						<p>MEDITATE</p>
+					</StyledLink>
+					<StyledLink
+						activeClass="active"
+						to="journals"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={1000}
+					>
+						<p>JOURNALS</p>
+					</StyledLink>
+				</StyledList>
+			</CustomNav>
+		</div>
+	);
 }
 
 export default Navigation;
