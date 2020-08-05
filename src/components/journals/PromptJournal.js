398fc3c894a573fca5e23540c64ea59632871d15
Garland Lai <garlandlai24@gmail.com>
2020-08-04 19:23:44 -0700

    create UI and button folder and file

--- src/components/journals/PromptJournal.js
+++ src/components/journals/PromptJournal.js
@@ -1,83 +1,106 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const StyledPrompt = styled.label`
-color: black !important;
-`
+	color: black !important;
+`;
 
 class PromptJournal extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      prompt1: "",
-      prompt2: "",
-      prompt3: "",
-      content: ""
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			prompt1: '',
+			prompt2: '',
+			prompt3: '',
+			content: '',
+		};
+	}
 
-  handleChange = (e) => {
-    this.setState({
-       [e.target.id]: e.target.value
-    })
-  }
+	handleChange = (e) => {
+		this.setState({
+			[e.target.id]: e.target.value,
+		});
+	};
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submitted")
-    const entry = {prompt1: this.state.prompt1, prompt2: this.state.prompt2, prompt3: this.state.prompt3, content: this.state.content}
-    axios.post('http://localhost:3000/entries',(entry))
-      .then(response => {
-        this.props.onUpdate();
-        this.props.clearForm();
-        console.log(response)
-      })
-      .catch(error => console.log(error))
-  }
+	handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('submitted');
+		const entry = {
+			prompt1: this.state.prompt1,
+			prompt2: this.state.prompt2,
+			prompt3: this.state.prompt3,
+			content: this.state.content,
+		};
+		axios
+			.post('http://localhost:3000/entries', entry)
+			.then((response) => {
+				this.props.onUpdate();
+				this.props.clearForm();
+				console.log(response);
+			})
+			.catch((error) => console.log(error));
+	};
 
-  render() {
-    console.log(this.state);
-    return (
-      <div className="container">
-        <form onSubmit={this.handleSubmit} id="addForm">
-          <h4 className="grey-text text-darken-3">Meditation Journal</h4>
-          <div className="input-field">
-            <StyledPrompt htmlFor="prompt1">How do you feel about your meditation experience?</StyledPrompt>
-            <input type="text" id="prompt1" onChange={this.handleChange} required/>
-          </div>
+	render() {
+		console.log(this.state);
+		return (
+			<div className="container">
+				<form onSubmit={this.handleSubmit} id="addForm">
+					<h4 className="grey-text text-darken-3">Meditation Journal</h4>
+					<div className="input-field">
+						<StyledPrompt htmlFor="prompt1">
+							How do you feel about your meditation experience?
+						</StyledPrompt>
+						<input
+							type="text"
+							id="prompt1"
+							onChange={this.handleChange}
+							required
+						/>
+					</div>
 
-          <div className="input-field">
-            <StyledPrompt htmlFor="prompt2">What did you notice about your thoughts?</StyledPrompt>
-            <input type="text" id="prompt2" onChange={this.handleChange} />
-          </div>
+					<div className="input-field">
+						<StyledPrompt htmlFor="prompt2">
+							What did you notice about your thoughts?
+						</StyledPrompt>
+						<input type="text" id="prompt2" onChange={this.handleChange} />
+					</div>
 
-          <div className="input-field">
-            <StyledPrompt htmlFor="prompt3">Did you notice any sensations or discomfort? Where?</StyledPrompt>
-            <input type="text" id="prompt3" onChange={this.handleChange} />
-          </div>
+					<div className="input-field">
+						<StyledPrompt htmlFor="prompt3">
+							Did you notice any sensations or discomfort? Where?
+						</StyledPrompt>
+						<input type="text" id="prompt3" onChange={this.handleChange} />
+					</div>
 
-          <div className="input-field">
-            <StyledPrompt htmlFor="content">This is where you can write anything you want!</StyledPrompt>
-            <textarea className="materialize-textarea"  id="content" onChange={this.handleChange} required/>
-          </div>
-          <div className="input-field">
-            <button type="submit">Create</button>
-            <button onClick={this.props.clearForm}>Cancel</button>
-          </div>
-        </form>
-      </div>
-    )
-  }
+					<div className="input-field">
+						<StyledPrompt htmlFor="content">
+							This is where you can write anything you want!
+						</StyledPrompt>
+						<textarea
+							className="materialize-textarea"
+							id="content"
+							onChange={this.handleChange}
+							required
+						/>
+					</div>
+					<div className="input-field">
+						<button type="submit">Create</button>
+						<button onClick={this.props.clearForm}>Cancel</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
 PromptJournal.propTypes = {
-  onNewEntry: PropTypes.func,
-  onUpdate: PropTypes.func,
-  promptVisibleOnPage: PropTypes.bool,
-  clearForm: PropTypes.func
+	onNewEntry: PropTypes.func,
+	onUpdate: PropTypes.func,
+	promptVisibleOnPage: PropTypes.bool,
+	clearForm: PropTypes.func,
 };
-
 
 export default PromptJournal;
